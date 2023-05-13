9526a0dcd9e2d9b02cdcc35b1c6ee16f8d44153c
ekoehn <edouard.koehn@idiap.ch>
2023-05-13 15:50:37 +0200

    workflow working

--- iar_project/segmentation.py
+++ iar_project/segmentation.py
@@ -1,7 +1,9 @@
+import os
+
 import cv2
 import numpy as np
 
-from iar_project.utils import DATA_PATH
+import iar_project.utils as utils
 
 
 def draw_contours(img, contours):
@@ -38,8 +40,8 @@ def clean_mask(mask, min_area=1000, max_area=1600):
         for cnt in contours
         if (cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < max_area)
     ]
-
     rects = [cv2.minAreaRect(cnt) for cnt in filtered_contours]
+    rects = [rect for rect in rects if (abs(rect[1][0] - rect[1][1]) < 0.1)]
     boxes = [cv2.boxPoints(rect) for rect in rects]
     boxes = [np.intp(box) for box in boxes]
     return boxes
@@ -52,7 +54,7 @@ def extract_segemented_object(src_img, mask, src_image_number):
             src_image_number: index of the images
     return  None
     """
-    path_out = DATA_PATH + "/segmentation_results"
+    path_out = utils.check_output_segmentation_folder()
     # Split the source image
     src_b, src_g, src_r = cv2.split(src_img)
     # Find the contour in the mask
@@ -60,34 +62,73 @@ def extract_segemented_object(src_img, mask, src_image_number):
 
     ind = 0
     for c in contours:
-        # Create the rectrangle
-        rectangleArea = cv2.minAreaRect(c)
-        angle = int(rectangleArea[2])
-        center = (int(rectangleArea[0][0]), int(rectangleArea[0][1]))
-        # Produce the rotated channel
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        r_rotated = cv2.warpAffine(
-            src_r, M, dsize=src_r.shape[1::-1], flags=cv2.INTER_LINEAR
-        )
-        g_rotated = cv2.warpAffine(
-            src_g, M, dsize=src_g.shape[1::-1], flags=cv2.INTER_LINEAR
-        )
-        b_rotated = cv2.warpAffine(
-            src_b, M, dsize=src_b.shape[1::-1], flags=cv2.INTER_LINEAR
-        )
-        # Define where to crop
-        min_y = np.min(c[:, :, 0])
-        max_y = np.max(c[:, :, 0])
-        min_x = np.min(c[:, :, 1])
-        max_x = np.max(c[:, :, 1])
-        # Crop each channel
-        segement_r = r_rotated[min_x : max_x + 1, min_y : max_y + 1]
-        segement_g = g_rotated[min_x : max_x + 1, min_y : max_y + 1]
-        segement_b = b_rotated[min_x : max_x + 1, min_y : max_y + 1]
+        if len(c) > 10:
+            # Create the rectrangle
+            rectangleArea = cv2.minAreaRect(c)
+            angle = int(rectangleArea[2])
+            center = (int(rectangleArea[0][0]), int(rectangleArea[0][1]))
+            box = cv2.boxPoints(rectangleArea)
+            box = np.intp(box)
+            # Rotate the image
+            M = cv2.getRotationMatrix2D(center, angle, 1)
+            r_rotated = cv2.warpAffine(src_r, M, src_r.shape[:2], cv2.WARP_INVERSE_MAP)
+            g_rotated = cv2.warpAffine(src_g, M, src_g.shape[:2], cv2.WARP_INVERSE_MAP)
+            b_rotated = cv2.warpAffine(src_b, M, src_b.shape[:2], cv2.WARP_INVERSE_MAP)
+
+            # Extract the rotated object
+            cropped_r = r_rotated[
+                np.min(box[:, 1]) : np.max(box[:, 1]),
+                np.min(box[:, 0]) : np.max(box[:, 0]),
+            ]
+            cropped_g = g_rotated[
+                np.min(box[:, 1]) : np.max(box[:, 1]),
+                np.min(box[:, 0]) : np.max(box[:, 0]),
+            ]
+            cropped_b = b_rotated[
+                np.min(box[:, 1]) : np.max(box[:, 1]),
+                np.min(box[:, 0]) : np.max(box[:, 0]),
+            ]
+
+            # Refine the extraction
+            if cropped_b.shape[0] >= 128 & cropped_b.shape[1] >= 128:
+                rows, cols = cropped_r.shape
+                center = [int((cols - 1) / 2.0), int((rows - 1) / 2.0)]
+                cropped_r = cropped_r[
+                    center[0] - 64 : center[0] + 64, center[1] - 64 : center[1] + 64
+                ]
+                cropped_g = cropped_g[
+                    center[0] - 64 : center[0] + 64, center[1] - 64 : center[1] + 64
+                ]
+                cropped_b = cropped_b[
+                    center[0] - 64 : center[0] + 64, center[1] - 64 : center[1] + 64
+                ]
+            if (
+                cropped_r.shape[0] != 128
+                or cropped_g.shape[0] != 128
+                or cropped_b.shape[0] != 128
+            ):
+                print("stop")
+                plt.figure()
+                plt.imshow(src_img)
+
+                plt.figure()
+                plt.imshow(mask)
+
+                plt.figure()
+                plt.imshow(cropped_r)
+
+                print(c)
+                print(len(c))
+                print(cropped_r.shape)
+                print(type(cv2.merge([cropped_r, cropped_g, cropped_b])))
+                plt.show()
         # Fuse the channel
-        segemented = cv2.merge([segement_r, segement_g, segement_b])
-        cv2.imwrite(f"{path_out}/{src_image_number}_{ind}.png", segemented)
-        ind += 1
+        segemented = cv2.merge([cropped_r, cropped_g, cropped_b])
+
+        if segemented.shape[0] == 128:
+            export_file = f"{path_out}/{src_image_number}_{ind}.png"
+            cv2.imwrite(export_file, segemented)
+            ind += 1
     return
 
 
@@ -97,10 +138,10 @@ def filter_1(src_imgages):
         cv2.cvtColor(data, cv2.COLOR_BGR2HSV)[:, :, 0] for data in src_imgages
     ]
     threshold_h = [
-        cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV)[1] for img in train_data_hue
+        cv2.threshold(img, 0.1, 255, cv2.THRESH_BINARY_INV)[1] for img in train_data_hue
     ]
     mask_clean = [
-        draw_contours(img, clean_mask(img, min_area=15000, max_area=16000))
+        draw_contours(img, clean_mask(img, min_area=15000, max_area=16384))
         for img in threshold_h
     ]
     return mask_clean
@@ -114,13 +155,14 @@ def filter_2(src_images):
     threshold_v_100 = [
         cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1] for img in train_data_v
     ]
+
     kernel = np.ones((5, 5), np.uint8)
     threshold_v_100 = [
         cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=3)
         for img in threshold_v_100
     ]
     mask_clean = [
-        draw_contours(img, clean_mask(img, min_area=15000, max_area=16000))
+        draw_contours(img, clean_mask(img, min_area=14000, max_area=20000))
         for img in threshold_v_100
     ]
     return mask_clean
@@ -139,7 +181,49 @@ def filter_3(src_images):
         for img in threshold_h_28
     ]
     mask_clean = [
-        draw_contours(img, clean_mask(img, min_area=15000, max_area=16000))
+        draw_contours(img, clean_mask(img, min_area=15000, max_area=16384))
         for img in threshold_h_28
     ]
     return mask_clean
+
+
+def filter_4(src_images):
+    train_data_hue = [
+        cv2.cvtColor(data, cv2.COLOR_RGB2HSV)[:, :, 0] for data in src_images
+    ]
+    threshold_h = [
+        cv2.threshold(img, 38, 255, cv2.THRESH_BINARY)[1] for img in train_data_hue
+    ]
+    kernel = np.ones((6, 6), np.uint8)
+    thresholded_h = [
+        cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=3)
+        for img in threshold_h
+    ]
+    mask_clean = [
+        draw_contours(img, clean_mask(img, min_area=15000, max_area=16384))
+        for img in thresholded_h
+    ]
+    return mask_clean
+
+
+import matplotlib.pyplot as plt
+
+import iar_project.utils as utils
+
+
+def plot_mask(images, masks, title="Test"):
+    plt.figure(figsize=(20, 7))
+    plt.suptitle(title)
+
+    for img, mask, ind in zip(images, masks, np.arange(15)):
+        r, g, b = cv2.split(img)
+        r = r * mask
+        g = g * mask
+        b = b * mask
+        color_img = cv2.merge([r, g, b])
+        plt.title(f"img {ind}")
+        plt.subplot(2, 8, ind + 1)
+        plt.imshow(color_img, cmap="gray")
+        plt.axis("off")
+    plt.tight_layout()
+    plt.show()
--- iar_project/utils.py
+++ iar_project/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 
 GENERAL_PATH = "/Users/edouardkoehn/Documents/GitHub/iar_project"
 DATA_PATH = GENERAL_PATH + "/data_project"
+SEGMENTATION_OUTPUT_PATH = DATA_PATH + "/segmentation_results"
 
 
 def import_train():
@@ -18,9 +19,16 @@ def import_train():
         )
         img_path = os.path.join(train_path, img_name)
         if os.path.exists(img_path):
-            img = cv2.imread(img_path)
+            img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img_list.append(img)
         else:
             print(f"Image {img_name} not found.")
     return img_list
+
+
+def check_output_segmentation_folder():
+    path_out = SEGMENTATION_OUTPUT_PATH
+    if not (os.path.exists(path_out)):
+        os.mkdir(path_out)
+    return path_out
