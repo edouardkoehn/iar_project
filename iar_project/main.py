24f32e278b3c7e522a59ae198e65b24de8009c05
ekoehn <edouard.koehn@idiap.ch>
2023-06-01 18:05:41 +0200

    Final merge

--- iar_project/main.py
+++ iar_project/main.py
@@ -1,15 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
+import iar_project.clustering as clustering
 import iar_project.CNN as cnn
 import iar_project.save_evaluation_files as save_eval
 import iar_project.segmentation as segmentation
+import iar_project.solve_puzzle as solving
 import iar_project.utils as utils
 
 # General config for the pipeline
 DATA_PATH = utils.GENERAL_PATH + "/data_project2/test2/"
 OUTPUT_PATH = utils.GENERAL_PATH + "/data_project2/"
 group_ID = 19
-nb_images = 12
+nb_images = 1
 DL = True
 
 # Load data
@@ -32,7 +35,23 @@ if not DL:
         solution.append(clusters)
 
         # Solve puzzle
-        solution.append(puzzles)
+        unsolved_puzzles = utils.load_unsolved_images(i, len(puzzles))
+        solved_puzzles = []
+        for unsolved_puzzle, ind in zip(
+            unsolved_puzzles, np.arange(0, len(unsolved_puzzles))
+        ):
+            print(f"        Solving puzzle _{i}_{ind}")
+            solved_puzzles.append(
+                solving.solve_puzzle(
+                    unsolved_puzzle,
+                    piece_size=128,
+                    population=100,
+                    generations=50,
+                    termination_threshold=30,
+                    verbose=False,
+                )
+            )
+        solution.append(solved_puzzles)
 
         # Export solution
         save_eval.export_solutions(i, solution, path=OUTPUT_PATH, group_id=group_ID)
@@ -40,13 +59,39 @@ if not DL:
 else:
     for i, im in enumerate(data):
         solution = []
+        # Segementation
         mask = segmentation.compute_segementation_filters([im])
         solution.append(mask[0])
         segmentation.extract_segemented_object(im, mask[0], i, dataset_use=2)
-        solution.append(np.ones((2, 2)))
+        solution.append(np.random.rand(2, 2))
+
+        # Feature extraction and clustering
         clusters = cnn.CNN_classification(i)
         solution.append(clusters)
-        solution.append(np.ones((2, 2)))
+        path_out = utils.check_output_clustering_folder(dataset_used=2)
+        _, puzzles = clustering.save_puzzles(clusters, path_out, i)
+
+        # Solve puzzle
+        unsolved_puzzles = utils.load_unsolved_images(i, len(puzzles))
+        solved_puzzles = []
+        for unsolved_puzzle, ind in zip(
+            unsolved_puzzles, np.arange(0, len(unsolved_puzzles))
+        ):
+            print(f"Solving puzzle _{i}_{ind}")
+            solved_puzzles.append(
+                solving.solve_puzzle(
+                    unsolved_puzzle,
+                    piece_size=128,
+                    population=100,
+                    generations=50,
+                    termination_threshold=30,
+                    verbose=False,
+                )
+            )
+
+        solution.append(solved_puzzles)
+
+        # Export
         save_eval.export_solutions(
             i, solution, path=OUTPUT_PATH, group_id=group_ID, CNN=DL
         )
